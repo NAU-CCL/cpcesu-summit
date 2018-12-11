e7bc7283e32730e961eb5b7b3eaa21b720c804a6
Colton Nunley <crn79@nau.edu>
2018-12-11 11:37:53 -0700

    Docs formatting

--- summit/libs/auth/models.py
+++ summit/libs/auth/models.py
@@ -1,10 +1,10 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-
-import uuid
-
-from summit.libs.models import AuditModel
-
-class User(AbstractUser, AuditModel):
-    full_name = models.CharField(max_length=300)
-    external_id = models.CharField(max_length=100,unique=True,blank=False,default=uuid.uuid4)
+# from django.db import models
+# from django.contrib.auth.models import AbstractUser
+#
+# import uuid
+#
+# from summit.libs.models import AuditModel
+#
+# class User(AbstractUser, AuditModel):
+#     full_name = models.CharField(max_length=300)
+#     external_id = models.CharField(max_length=100,unique=True,blank=False,default=uuid.uuid4)
