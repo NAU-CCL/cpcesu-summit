6f53d52f08f2dcf000164981bc2676337dfc62fa
--replace-all <--replace-all>
2019-01-26 17:26:08 -0700

    Added model form functionality. Still needs to add objects to database.

--- summit/apps/projects/urls.py
+++ summit/apps/projects/urls.py
@@ -2,11 +2,11 @@
 # TODO: Set up urls to point to the projects views.py
 from config.links import link, get_name
 
-from .views import ProjectListView
+from . import views
 
 app_name = 'summit.apps.projects'
 urlpatterns = [
-    link(r'^$', ProjectListView.as_view(), name=get_name(app_name, 'Projects'), link_args={
+    link(r'^$', views.ProjectListView.as_view(), name=get_name(app_name, 'Projects'), link_args={
         'auth_required': True
     }),
 ]
