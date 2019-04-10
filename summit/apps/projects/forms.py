aa2be9aa2224fe9487909b80cb8e368651f0a236
Jasque Saydyk <jrs496@nau.edu>
2019-04-10 09:31:46 -0700

    Preparing for merge

--- summit/apps/projects/forms.py
+++ summit/apps/projects/forms.py
@@ -1,11 +1,11 @@
 from django import forms
-from .models import Project, File
+from .models import Project, File#, ProjectFiles
 
 
 class ProjectForm(forms.ModelForm):
     class Meta:
         model = Project
-        fields = ['p_num', 'project_title', 'short_summary', 'status', 'partner',
+        fields = ['project_title', 'short_summary', 'status', 'partner',
                   'federal_agency', 'cesu_unit', 'description', 'budget',
                   'student_support', 'vet_support']
         widgets = {
