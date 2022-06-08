775181a5036870628ba6e90474f5b75ff200c76e
Lucina de la Rosa <dld297@nau.edu>
2022-06-07 23:34:34 -0400

    fix to user additions

--- add-roles.py
+++ add-roles.py
@@ -7,9 +7,11 @@ ex = CESU.objects.get(id=2)
 
 CESURole.objects.all().delete()
 for user in User.objects.all():
+  user.user_cesus.add(summit)
   new_role = CESURole.objects.get_or_create(user_id = user.id, cesu_id=summit.id)[0]
   new_role.role = 'USER'
   new_role.save()
+  user.user_cesus.add(ex)
   new_role = CESURole.objects.get_or_create(user_id = user.id, cesu_id=ex.id)[0]
   new_role.role = 'USER'
   new_role.save()
